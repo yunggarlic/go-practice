1f40cf88bcbdfcdefd0004588333a3a3114cbe6d
Tim Ferrari <tim.ferrari@criticalmass.com>
2023-07-02 00:34:34 -0500

    renders templates

--- web-app/main.go
+++ web-app/main.go
@@ -1,52 +1,33 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 // Home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to my home page")
-
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(12, 17)
-	fmt.Fprintf(w, fmt.Sprintf("I am %v years old", sum))
 
 }
 
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0, 10)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by zero")
+		log.Println("Error executing template:", err)
 		return
 	}
-	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
-}
-
-func divideValues(x, y float32) (float32, error) {
-	var quotient float32
-	if y == 0 {
-		return quotient, errors.New("Cannot divide by zero")
-	}
-	quotient = x / y
-	return quotient, nil
-}
-
-// Adds two ints and returns the sum
-func addValues(x, y int) int {
-	return x + y
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	log.Println("Listening on port 3000")
 	http.ListenAndServe(":3000", nil)
